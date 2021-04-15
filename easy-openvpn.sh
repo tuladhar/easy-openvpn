18f4f103616d2194f3d92294e8902765513df8e6
Puru <tuladharpuru@gmail.com>
2021-04-15 12:46:14 +0545

    Update easy-openvpn.sh

--- easy-openvpn.sh
+++ easy-openvpn.sh
@@ -11,25 +11,24 @@ set -o pipefail
 NAME=
 OPENVPN_ENDPOINT=
 CLIENT=
-
-OVPN_DATA="ovpn-data"
+OVPN_DATA=
 
 function install_docker {
-#	which docker > /dev/null && return
+	which docker > /dev/null && return
 	curl -fsSL https://get.docker.com -o get-docker.sh
 	sudo sh get-docker.sh
 }
 
 function init_openvpn {
-#	sudo docker volume inspect $OVPN_DATA > /dev/null && return
+	sudo docker volume inspect $OVPN_DATA > /dev/null && return
 	sudo docker volume create --name $OVPN_DATA
 	sudo docker run -v $OVPN_DATA:/etc/openvpn --rm kylemanna/openvpn ovpn_genconfig -u udp://$OPENVPN_ENDPOINT
 	sudo docker run -v $OVPN_DATA:/etc/openvpn --rm -it -e "EASYRSA_BATCH=1" -e "EASYRSA_REQ_CN=CA" kylemanna/openvpn ovpn_initpki nopass
 }
 
 function install_openvpn_systemd {
-#	[ -f /etc/systemd/system/docker-openvpn@$NAME.service ] && return
-# is-active?
+	[ -f /etc/systemd/system/docker-openvpn@$NAME.service ] && return
+	# Maybe use? is-active?
 	curl -L https://raw.githubusercontent.com/kylemanna/docker-openvpn/master/init/docker-openvpn%40.service | sudo tee /etc/systemd/system/docker-openvpn@$NAME.service
 	sudo systemctl enable --now docker-openvpn@$NAME.service
 	sudo systemctl status docker-openvpn@$NAME.service
@@ -39,7 +38,6 @@ function generate_client_config {
 	sudo docker run -v $OVPN_DATA:/etc/openvpn --rm -it kylemanna/openvpn easyrsa build-client-full $CLIENT nopass
 	sudo docker run -v $OVPN_DATA:/etc/openvpn --rm kylemanna/openvpn ovpn_getclient $CLIENT > $CLIENT.ovpn
 	echo "OpenVPN client configuration successfully saved as $CLIENT.ovpn"
-	exit 0
 }
 
 flags=(
@@ -77,17 +75,24 @@ while true; do
 	esac
 done
 
-if [[ ! -z $CLIENT ]]; then
-        generate_client_config
-fi
-
-if [[ -z $NAME || -z $OPENVPN_ENDPOINT ]]; then
-	echo -e "Install OpenVPN:\n\t$0 --name [NAME] --endpoint [OPENVPN_ENDPOINT]"
-	echo
-	echo -e "Generate and Download Client Configuration:\n\t$0 --client [NAME]"
+if [[ -z $NAME ]]; then
+	echo -e "Error: --name is required"
 	exit 1
 fi
 
+OVPN_DATA="ovpn-data-$NAME"
+
+if [[ ! -z $CLIENT ]]; then
+        generate_client_config
+	exit 0
+fi
+
+if [[ -z $OPENVPN_ENDPOINT ]]; then
+	echo -e "Install OpenVPN:\n\t$0 --name [NAME] --endpoint [OPENVPN_ENDPOINT]"
+	echo
+	echo -e "Generate and Download Client Configuration:\n\t$0 --name [NAME] --client [CLIENT_NAME]"
+	exit 1
+fi
 
 install_docker
 init_openvpn
